c0cf653f0ae6e0485dd556d649e85cae1280c30d
Alex <alex.rodrigues.developer@gmail.com>
2021-03-06 20:54:12 -0300

    faznedo dump da app store

--- reviews.py
+++ reviews.py
@@ -1,7 +1,16 @@
 from app_store_scraper import AppStore
 from pprint import pprint
+import json
 
-appStoreXp = AppStore(country="br", app_name="xpinvestimentos")
-appStoreXp.review(how_many=20)
+app_store = AppStore(country="br", app_name="xpinvestimentos")
+app_store.review(how_many=20)
+app_store_xp = app_store.reviews
 
-pprint(appStoreXp.reviews)
+for review in app_store_xp:
+    date = review['date'].strftime("%Y-%m-%d %H:%M:%S")
+    review['date'] = date
+
+pprint(app_store_xp)
+
+with open('appstore.json', 'w', encoding='utf-8') as f:
+    json.dump(app_store_xp, f,  ensure_ascii=False, indent=4)
